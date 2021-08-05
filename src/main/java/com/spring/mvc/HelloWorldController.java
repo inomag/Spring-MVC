922f946bc3dbc8145c2929dd760dcea77dcf55c5
Inomag <pratiksr12345@gmail.com>
2021-08-06 01:10:30 +0530

    RequestParam

--- src/main/java/com/spring/mvc/HelloWorldController.java
+++ src/main/java/com/spring/mvc/HelloWorldController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -24,9 +25,16 @@ public class HelloWorldController {
 	
 	
 	// Controller method to read data & add data to model
+//	@RequestMapping("/processFormV2")
+//	public String processv2(HttpServletRequest request, Model model) {
+//		String name = request.getParameter("studentName");
+//		name = name.toUpperCase();
+//		model.addAttribute("message", "Hello "+name);
+//		return "helloworld";
+//	}
+	
 	@RequestMapping("/processFormV2")
-	public String processv2(HttpServletRequest request, Model model) {
-		String name = request.getParameter("studentName");
+	public String processv2(@RequestParam("studentName")String name, Model model) {
 		name = name.toUpperCase();
 		model.addAttribute("message", "Hello "+name);
 		return "helloworld";
